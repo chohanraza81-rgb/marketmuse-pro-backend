2c63bd53b97077d62e4b7a75bc6b83591d883162
chohanraza81-rgb <chohanraza81@gmail.com>
2026-08-18 02:29:47 +0500

    Update google-trends-api.d.ts

--- src/types/google-trends-api.d.ts
+++ src/types/google-trends-api.d.ts
@@ -1,7 +1,7 @@
 declare module 'google-trends-api' {
   export function interestOverTime(options: {
     keyword: string;
-    startTime: Date;
+    startTime?: Date;
     endTime?: Date;
     geo?: string;
     hl?: string;
@@ -11,48 +11,9 @@ declare module 'google-trends-api' {
     resolution?: string;
   }): Promise<string>;
 
-  export function interestByRegion(options: {
-    keyword: string;
-    startTime?: Date;
-    endTime?: Date;
-    geo?: string;
-    hl?: string;
-    timezone?: number;
-    category?: number;
-    resolution?: string;
-  }): Promise<string>;
-
-  export function relatedQueries(options: {
-    keyword: string;
-    startTime?: Date;
-    endTime?: Date;
-    geo?: string;
-    hl?: string;
-    timezone?: number;
-    category?: number;
-  }): Promise<string>;
-
-  export function relatedTopics(options: {
-    keyword: string;
-    startTime?: Date;
-    endTime?: Date;
-    geo?: string;
-    hl?: string;
-    timezone?: number;
-    category?: number;
-  }): Promise<string>;
-
-  export function dailyTrends(options: {
-    trendDate?: Date;
-    geo?: string;
-    hl?: string;
-    timezone?: number;
-  }): Promise<string>;
-
-  export function realTimeTrends(options: {
-    geo?: string;
-    hl?: string;
-    timezone?: number;
-    category?: string;
-  }): Promise<string>;
+  export function interestByRegion(options: any): Promise<string>;
+  export function relatedQueries(options: any): Promise<string>;
+  export function relatedTopics(options: any): Promise<string>;
+  export function dailyTrends(options: any): Promise<string>;
+  export function realTimeTrends(options: any): Promise<string>;
 }
